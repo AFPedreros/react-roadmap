3a954d870e02d94678ca9ae343523498149e8695
AFPedreros <felipe.pedreros94@gmail.com>
2024-02-04 10:57:55 -0500

    Update styles  of code-block component

--- src/app.tsx
+++ src/app.tsx
@@ -7,7 +7,7 @@ import { CodeBlock } from '@/components/code-block';
 export default function App() {
 	return (
 		<div className="grid grid-cols-12 ">
-			<div className="relative z-10 mt-8 hidden overflow-visible pr-4 lg:col-span-2 lg:block"></div>
+			<div className="relative z-10 hidden pr-4 mt-8 overflow-visible lg:col-span-2 lg:block"></div>
 			<div className="z-50 col-span-12 mt-10 lg:col-span-10 lg:px-16 xl:col-span-8">
 				<div className="w-full space-y-6">
 					<Title>
@@ -18,12 +18,18 @@ export default function App() {
 					<Tabs color="secondary" aria-label="Tabs colors" radius="full">
 						<Tab key="explanation" title="Explicación">
 							<div className="space-y-4">
-								<CodeBlock code={steps[1]} />
-								<CodeBlock code={steps[2]} />
+								<CodeBlock code={steps[1]} className="border rounded-lg" highlightLines={['1,5']} />
+								<CodeBlock
+									code={steps[2]}
+									className="border rounded-lg bg-background"
+									copy
+									highlightLines={['3-7']}
+									highlightColor="danger"
+								/>
 							</div>
 						</Tab>
 						<Tab
-							className="flex w-full max-w-screen-md justify-center"
+							className="flex justify-center w-full max-w-screen-md"
 							key="example"
 							title="Ejemplo"
 						>
@@ -33,7 +39,7 @@ export default function App() {
 					</Tabs>
 				</div>
 			</div>
-			<div className="z-10 mt-8 hidden pl-4 xl:col-span-2 xl:flex"></div>
+			<div className="z-10 hidden pl-4 mt-8 xl:col-span-2 xl:flex"></div>
 		</div>
 	);
 }
--- src/components/code-block.tsx
+++ src/components/code-block.tsx
@@ -3,24 +3,69 @@ import { CopyButton } from '@/components/copy-button';
 import { cn } from '@nextui-org/react';
 import { Code } from '@nextui-org/react';
 
+type HighlightColor = 'primary' | 'success' | 'danger' | 'warning' | 'secondary';
+
 type CodeBlockProps = {
 	code: string;
 	copy?: boolean;
+	className?: string;
+	highlightLines?: string[];
+	highlightColor?: HighlightColor;
 };
 
-export function CodeBlock({ code, copy }: CodeBlockProps) {
+export function CodeBlock({
+	code,
+	copy,
+	className,
+	highlightLines,
+	highlightColor = 'primary'
+}: CodeBlockProps) {
+	const processHighlightLines = (highlightLines: string[]): number[] => {
+		let linesToHighlight: number[] = [];
+
+		highlightLines.forEach((line) => {
+			if (line.includes('-')) {
+				const [start, end] = line.split('-').map(Number);
+				for (let i = start; i <= end; i++) {
+					linesToHighlight.push(i);
+				}
+			} else if (line.includes(',')) {
+				line.split(',').forEach((num) => {
+					linesToHighlight.push(parseInt(num, 10));
+				});
+			} else {
+				linesToHighlight.push(parseInt(line, 10));
+			}
+		});
+
+		return linesToHighlight;
+	};
+
+	const linesToHighlight = processHighlightLines(highlightLines || []);
+	const backgroundColorClass = `bg-${highlightColor}`;
+
+	console.log('highLightColor', `${backgroundColorClass}/25`);
+
 	return (
 		<Highlight theme={themes.oneDark} code={code} language="tsx">
 			{({ tokens, getTokenProps }) => (
-				<div className="relative overflow-hidden rounded-xl border">
-					<div className="relative flex items-center justify-center  py-4">
+				<div className={cn(className, 'overflow-hidden')}>
+					<div className="relative flex items-center justify-center py-4">
 						<Code color="primary">counter.tsx</Code>
-						{!copy && <CopyButton className="absolute right-3 top-3" value={code} />}
+						{!!copy && <CopyButton className="absolute right-3 top-3" value={code} />}
 					</div>
 					<pre className="pb-4">
 						{tokens.map((line, i) => (
-							<div className={cn(i === 2 ? 'bg-secondary/25' : '', 'relative px-4')} key={i}>
-								{i === 2 && <div className=" absolute left-0 h-full w-1 bg-secondary" />}
+							<div
+								className={cn(
+									linesToHighlight.includes(i + 1) ? backgroundColorClass : '',
+									'relative bg-opacity-25 px-4'
+								)}
+								key={i}
+							>
+								{linesToHighlight.includes(i + 1) && (
+									<div className={cn(backgroundColorClass, 'absolute left-0 h-full w-1')} />
+								)}
 								<span className="mr-2">{i + 1}</span>
 								{line.map((token, key) => (
 									<span key={key} {...getTokenProps({ token })} />
